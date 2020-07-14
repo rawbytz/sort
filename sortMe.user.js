f379e6f9a29d6c7d5478d6356aad7555d0b6ffeb
rawbytz <rawbytz@gmail.com>
2020-07-14 16:37:31 -0600

    v3.5 convert to addButton, change button color

--- sortMe.user.js
+++ sortMe.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Sort WorkFlowy
 // @namespace    https://rawbytz.wordpress.com
-// @version      3.4.1
+// @version      3.5
 // @description  Use Ctrl+Shift+S to sort the current zoom level children.
 // @author       rawbytz
 // @match        https://workflowy.com/*
-// @match        https://beta.workflowy.com/*
+// @match        https://*.workflowy.com/*
 // @updateUrl    https://github.com/rawbytz/sort/raw/master/sortMe.user.js
 // @downloadUrl  https://github.com/rawbytz/sort/raw/master/sortMe.user.js
 // @grant        none
@@ -34,10 +34,11 @@
       }, 50);
     }
     const htmlEscText = str => str.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;");
-    function showSortDialog(bodyHtml, title, button1, button2) {
-      const style = '.btnX{font-size:18px;background-color:#49baf2;border:2px solid;border-radius:20px;color:#fff;padding:5px 15px;margin-top:16px;margin-right:16px}.btnX:focus{border-color:#c4c4c4}';
-      const buttons = `<div><button type="button" class="btnX" id="btn1">${button1}</button><button type="button" class="btnX" id="btn2">${button2}</button></div>`;
-      WF.showAlertDialog(`<style>${htmlEscText(style)}</style><div>${bodyHtml}</div>${buttons}`, title);
+    function showSortDialog(bodyHtml, title) {
+      const addButton = (num, name) => `<button type="button" class="btnX" id="btn${num.toString()}">${name}</button>`;
+      const style = '.btnX{font-size:18px;background-color:steelblue;border:2px solid;border-radius:20px;color:#fff;padding:5px 15px;margin-top:16px;margin-right:16px}.btnX:focus{border-color:#c4c4c4}';
+      const buttons = addButton(1, "A-Z") + addButton(2, "Z-A");
+      WF.showAlertDialog(`<style>${htmlEscText(style)}</style><div>${bodyHtml}</div><div>${buttons}</div>`, title);
       setTimeout(() => {
         const btn1 = document.getElementById("btn1");
         const btn2 = document.getElementById("btn2");
@@ -47,16 +48,14 @@
       }, 100);
     }
     const canCreateChild = item => !item.isReadOnly() || item.isMainDocumentRoot() || (item.isAddedSubtreePlaceholder() && !item.data.added_subtree.isReadOnly());
-    if (WF.currentSearchQuery()) {
-      return void toastMsg("Sorting is disabled when search is active.", 3, true);
-    }
+    if (WF.currentSearchQuery()) return void toastMsg("Sorting is disabled when search is active.", 3, true);
     const parent = WF.currentItem();
     if (!canCreateChild(parent)) return void toastMsg("Read-Only. Cannot sort bullets.", 3, true);
     const children = parent.getChildren();
     if (children.length < 2) return void toastMsg("Nothing to sort.", 3, true);
     if (children.length > maxChildren) return void toastMsg(`Sorting more than ${maxChildren} children upsets the WorkFlowy gods, and has been disabled.`, 5, true);
     const sortInfo = `Sort <b>${children.length}</b> children?`;
-    showSortDialog(sortInfo, parent.getNameInPlainText(), 'A-Z', 'Z-A');
+    showSortDialog(sortInfo, parent.getNameInPlainText());
   }
   // Ctrl+Shft+S
   document.addEventListener("keydown", function (event) {
